326ed35241d387cb39b1d70ae0f75e97c6fccd71
mrsudarshan <rrraishati@gmail.com>
2020-09-25 03:00:06 +0545

    removed  jquery as dependency, now all  vanilla js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-function startTracking(){
-// WorldWide
+function startTracking() {
+	// WorldWide
 	let ta1 = document.getElementById("ta1");
 	let nc1 = document.getElementById("tc1");
 	let nd1 = document.getElementById("td1");
@@ -7,7 +7,7 @@ function startTracking(){
 	let ts1 = document.getElementById("ts1");
 	let tu1 = document.getElementById("tu1");
 	let td1 = document.getElementById("td1");
-// Local
+	// Local
 	let ta2 = document.getElementById("ta2");
 	let nc2 = document.getElementById("tc2");
 	let nd2 = document.getElementById("td2");
@@ -15,21 +15,50 @@ function startTracking(){
 	let ts2 = document.getElementById("ts2");
 	let tu2 = document.getElementById("tu2");
 	let td2 = document.getElementById("td2");
-	worldwideData();
 }
 
-$.get("https://api.ipify.org/?format=json", {"ip":"data"})
-	.done(function(getIP){
-		fetch("https://ipapi.co/"+getIP.ip+"/country_code")
-			.then(response => response.text())
-			.then((response) => {
-				localData(response)
-			})
-	});
+const getLocalData = async (country) => {
+	const localData = await fetch(`https://api.thevirustracker.com/free-api?countryTotal=${country}`);
+	return await localData.json();
+}
 
-function worldwideData(){
-	$.get("https://api.thevirustracker.com/free-api?global=stats", function(getWorldWide){
-		// console.log(getWorldWide.results);
+const fetchCountry = async (ip) => {
+	const country = await fetch(`https://ipapi.co/${ip}/country_code`)
+	return await country.text();
+}
+
+const getIp = async () => {
+	const ip = await fetch('https://api.ipify.org/?format=json')
+	return await ip.json();
+}
+
+getIp()
+	.then(ip => {
+		fetchCountry(ip.ip)
+			.then(country => {
+				getLocalData(country)
+					.then(getLocal => {
+						let lD = getLocal.countrydata[0];
+						ta2.innerHTML = lD.total_active_cases;
+						nc2.innerHTML = lD.total_new_cases_today;
+						nd2.innerHTML = lD.total_new_deaths_today;
+						tc2.innerHTML = lD.total_cases;
+						ts2.innerHTML = lD.total_serious_cases;
+						tr2.innerHTML = lD.total_recovered;
+						tu2.innerHTML = lD.total_unresolved;
+						td2.innerHTML = lD.total_deaths;
+					})
+			})
+	})
+
+
+
+const worldwideData = async () => {
+	const getWorldWide = await fetch(`https://api.thevirustracker.com/free-api?global=stats`);
+	return await getWorldWide.json();
+}
+worldwideData()
+	.then(getWorldWide => {
 		let wwD = getWorldWide.results[0];
 		ta1.innerHTML = wwD.total_active_cases;
 		nc1.innerHTML = wwD.total_new_cases_today;
@@ -39,20 +68,4 @@ function worldwideData(){
 		tr1.innerHTML = wwD.total_recovered;
 		tu1.innerHTML = wwD.total_unresolved;
 		td1.innerHTML = wwD.total_deaths;
-	});
-}
-
-function localData(country){
-	$.get("https://api.thevirustracker.com/free-api?countryTotal="+country, function(getLocal){
-		// console.log(getLocal.countrydata);
-		let lD = getLocal.countrydata[0];
-		ta2.innerHTML = lD.total_active_cases;
-		nc2.innerHTML = lD.total_new_cases_today;
-		nd2.innerHTML = lD.total_new_deaths_today;
-		tc2.innerHTML = lD.total_cases;
-		ts2.innerHTML = lD.total_serious_cases;
-		tr2.innerHTML = lD.total_recovered;
-		tu2.innerHTML = lD.total_unresolved;
-		td2.innerHTML = lD.total_deaths;
-	});
-}
+	})
